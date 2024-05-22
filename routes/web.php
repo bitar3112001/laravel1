7c3e699fc9696da922ed3f1d235c09a6f7792fd7
Mahdi <mkaalayan@gmail.com>
2024-05-22 17:23:07 +0300

    first-task

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return 'test';
+Route::get('/test2', function () {
+    return 'test2';
 });
 
